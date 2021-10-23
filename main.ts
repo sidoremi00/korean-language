9f32aeebbd5b4d627c337d63a11d1d9b3ef0a3a1
sidoremi00 <92983634+sidoremi00@users.noreply.github.com>
2021-10-23 12:29:25 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,336 @@
+function ㅌ () {
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        # . . . .
+        # # # # #
+        `)
+}
+function ㅣ () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+}
+function ㅐ () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
+}
+function ㅑ () {
+    basic.showLeds(`
+        . . # . .
+        . . # # .
+        . . # . .
+        . . # # .
+        . . # . .
+        `)
+}
+function ㅁ () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+}
+function ㅛ () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+}
+function ㅅ () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . # . # .
+        # . . . #
+        # . . . #
+        `)
+}
+function ㅏ () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # # .
+        . . # . .
+        . . # . .
+        `)
+}
+function ㅔ () {
+    basic.showLeds(`
+        . . # . #
+        . . # . #
+        . # # . #
+        . . # . #
+        . . # . #
+        `)
+}
+function ㄷ () {
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # . . . .
+        # . . . .
+        # # # # #
+        `)
+}
+function ㅍ () {
+    basic.showLeds(`
+        # # # # #
+        . # . # .
+        . # . # .
+        . # . # .
+        # # # # #
+        `)
+}
+function 띄어쓰기 () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+}
+function _ () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        `)
+}
+function ㄹ () {
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        # # # # #
+        # . . . .
+        # # # # #
+        `)
+}
+function ㅇ () {
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+}
+function _3 () {
+    basic.showLeds(`
+        . # # # .
+        . . . # .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
+}
+function _4 () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
+}
+function ㅠ () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        . # . # .
+        . . . . .
+        `)
+}
+function ㅂ () {
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        # # # # #
+        # . . . #
+        # # # # #
+        `)
+}
+function ㅈ () {
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+}
+function ㅡ () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+}
+function ㅖ () {
+    basic.showLeds(`
+        . . # . #
+        . # # . #
+        . . # . #
+        . # # . #
+        . . # . #
+        `)
+}
+function ㅎ () {
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        . . # . .
+        `)
+}
+function ㅚ () {
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . # . #
+        . . # . #
+        # # # # #
+        `)
+}
+function _2 () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # . .
+        `)
+}
+function ㅟ () {
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . . . #
+        # # # # #
+        . . # . #
+        `)
+}
+function ㅒ () {
+    basic.showLeds(`
+        . # . # .
+        . # # # .
+        . # . # .
+        . # # # .
+        . # . # .
+        `)
+}
+function ㅓ () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . # # . .
+        . . # . .
+        . . # . .
+        `)
+}
+function ㅋ () {
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        # # # # #
+        . . . . #
+        . . . . #
+        `)
+}
+function ㅙ () {
+    basic.showLeds(`
+        . . . # #
+        . . . # #
+        . # . # #
+        . # . # #
+        # # # # #
+        `)
+}
+function ㅜ () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        . . # . .
+        . . . . .
+        `)
+}
+function ㄴ () {
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # # # # #
+        `)
+}
+function ㅊ () {
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+}
+function ㅕ () {
+    basic.showLeds(`
+        . . # . .
+        . # # . .
+        . . # . .
+        . # # . .
+        . . # . .
+        `)
+}
+function ㄱ () {
+    basic.showLeds(`
+        # # # # #
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        `)
+}
+function ㅗ () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+}
+// 주석~~~~
+ㅇ()
+ㅏ()
+ㄴ()
+띄어쓰기()
+ㄴ()
+ㅕ()
+ㅇ()
+_4()
+basic.forever(function () {
+	
+})
